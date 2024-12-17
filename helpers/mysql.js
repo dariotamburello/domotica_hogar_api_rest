075a43d9a72dd529aaaae20184acaa8f08c7a733
Dario Tamburello <dariotamburello@hotmail.com>
2024-12-17 11:18:39 -0300

    fix timeout mysql problem

--- helpers/mysql.js
+++ helpers/mysql.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise'
 
 import { DBNAME, DBPASS, DBPORT, DBSERVER, DBUSER } from '../config.js'
 
-export const conn = await mysql.createConnection({
+export const conn = mysql.createPool({
   host: DBSERVER,
   port: DBPORT,
   user: DBUSER,
